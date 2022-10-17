394d76dafb7629c371216730af5054ceef2855d3
mehdi-harakat <mehdiharakat20@gmail.com>
2022-10-17 19:34:49 +0100

    another way to hide and show nav in page

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,16 +1,23 @@
-let myElem = document.getElementById("valHala");
+let myElem = document.getElementById( "valHala" );
+let myNav = document.getElementById( "navMenu" );
 
-myElem.addEventListener("focus", myFocus, true);
-myElem.addEventListener("blur", myBlur, true);
 
-function myFocus() {
-  document.querySelector(
-    "header .container .main-nav .nav:last-child .container"
-  ).style.cssText = "top: 78px; opacity: 1; pointer-events:painted";
-}
+// myElem.addEventListener("focus", myFocus, true);
+// myElem.addEventListener("blur", myBlur, true);
 
-function myBlur() {
-  document.querySelector(
-    "header .container .main-nav .nav:last-child .container"
-  ).style.cssText = "top: 100px; opacity: 0; pointer-events:none";
+// function myFocus() {
+//   document.querySelector(
+//     "header .container .main-nav .nav:last-child .container"
+//   ).style.cssText = "top: 78px; opacity: 1; pointer-events:painted";
+// }
+
+// function myBlur() {
+//   document.querySelector(
+//     "header .container .main-nav .nav:last-child .container"
+//   ).style.cssText = "top: 100px; opacity: 0; pointer-events:none";
+// }
+
+
+myElem.onclick = function () {
+  myNav.classList.toggle( 'helloHello' );
 }
