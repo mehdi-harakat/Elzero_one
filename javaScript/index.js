6bbaf1c94cf61fb5d23e72491cc7b9ffda3caba4
mehdi-harakat <mehdiharakat20@gmail.com>
2022-10-18 03:46:14 +0100

    create more ways for nav bar

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,6 +1,18 @@
 let myElem = document.getElementById( "valHala" );
 let myNav = document.getElementById( "navMenu" );
 
+// -----------------------------------------------------------FIRST WAY-----------------------------------------------
+
+myElem.addEventListener( "focus", function () {
+  myNav.classList.toggle( 'helloHello' );
+}, true );
+
+myElem.addEventListener( "blur", function () {
+  myNav.classList.remove( 'helloHello' );
+}, true );
+
+
+// -----------------------------------------------------------SECOND WAY-----------------------------------------------
 
 // myElem.addEventListener("focus", myFocus, true);
 // myElem.addEventListener("blur", myBlur, true);
@@ -17,7 +29,28 @@ let myNav = document.getElementById( "navMenu" );
 //   ).style.cssText = "top: 100px; opacity: 0; pointer-events:none";
 // }
 
+// -----------------------------------------------------------THIRD WAY-------------------------------------------------
 
-myElem.onclick = function () {
-  myNav.classList.toggle( 'helloHello' );
-}
+// myElem.addEventListener("focus", myFocus, true);
+// myElem.addEventListener("blur", myBlur, true);
+
+// function myFocus() {
+//   myNav.classList.toggle( 'helloHello' );
+// }
+
+// function myBlur() {
+//   myNav.classList.remove( 'helloHello' );
+// }
+
+// -----------------------------------------------------------FOURTH WAY------------------------------------------------
+
+// myElem.onclick = function () {
+//   myNav.classList.toggle( 'helloHello' );
+// }
+
+// myElem.addEventListener( "blur", myBlur, true );
+
+// function myBlur() {
+//   myNav.classList.remove( 'helloHello' );
+//   console.log( 'Hello' );
+// };
