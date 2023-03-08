14a4554c8663babdaa9bc40514688abeb8aaa908
mehdi-harakat <mehdiharakat20@gmail.com>
2023-03-08 03:31:19 +0100

    add counter

--- javaScript/index.js
+++ javaScript/index.js
@@ -30,8 +30,7 @@ let one = document.querySelectorAll(".main-list");
 let hello = document.querySelectorAll(".newCounter");
 let trust = false;
 
-
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
   if (window.scrollY >= container.offsetTop - 300) {
     one.forEach(function (elem) {
       containerAll(elem);
@@ -44,7 +43,7 @@ window.addEventListener('scroll', function() {
       trust = true;
     }
   }
-})
+});
 
 function containerAll(test) {
   let targetting = test.parentElement.dataset.couter;
@@ -61,7 +60,6 @@ function helloCounter(test) {
   }, 3000 / targetting);
 }
 
-
 // --------------------------------------------------------- End The Scroll Function ---------------------------------------------
 
 // --------------------------------------------------------- Start The Function For Our Stats Section ---------------------------
@@ -70,7 +68,7 @@ let ourStats = document.querySelector(".our-stats");
 let elementH = document.querySelectorAll(".our-stats .counting");
 let startTow = false;
 
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
   if (window.scrollY >= ourStats.offsetTop - 400) {
     if (!startTow) {
       elementH.forEach((elem) => {
@@ -85,4 +83,40 @@ window.addEventListener('scroll', function() {
     }
     startTow = true;
   }
-})
+});
+
+// ---------------------------------------------- Start Counter ---------------------------------
+
+let days = document.querySelector(".days");
+let hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+const dateStart = new Date("Apr 30, 2023 23:59:59");
+
+const counter = setInterval(() => {
+  const dateNow = new Date().getTime();
+  let dateDeff = dateStart - dateNow;
+
+  let daysCount = Math.floor(dateDeff / (1000 * 60 * 60 * 24));
+  let hoursCount = Math.floor(
+    (dateDeff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+
+  let minutesCount = Math.floor(
+    ((dateDeff % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) / (1000 * 60)
+  );
+
+  let secondesCount = Math.floor(
+    (((dateDeff % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) % (1000 * 60)) /
+      1000
+  );
+
+  days.innerHTML = daysCount < 10 ? `0${daysCount}` : daysCount;
+  hours.innerHTML = hoursCount < 10 ? `0${hoursCount}` : hoursCount;
+  minutes.innerHTML = minutesCount < 10 ? `0${minutesCount}` : minutesCount;
+  seconds.innerHTML = secondesCount < 10 ? `0${secondesCount}` : secondesCount;
+
+  if (dateDeff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
